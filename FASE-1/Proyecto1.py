701fedc8e24a75601715edc431ee87251f5b491d
Sandy-0520 <sandy-acosta2011@hotmail.com>
2024-08-29 02:03:21 -0500

    Initial commit - Upload of complete project

---
+++ FASE-1/Proyecto1.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""proyecto 1.ipynb
+
+Sandy Isabel Acosta Diaz
+
+Original file is located at
+    https://es.wikipedia.org/wiki/Anexo:Compa%C3%B1%C3%ADas_del_S%26P_500
+"""
+
+
+
+import yfinance as yf
+import pandas as pd
+import os
+import logging
+
+
+log_dir = './logs'
+data_dir = './data'
+
+
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
+
+log_filename = os.path.join(log_dir, 'etl_process.log')
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler(log_filename),
+        logging.StreamHandler()
+    ]
+)
+
+def extract_data(ticker, start_date, end_date):
+    try:
+        logging.info(f'Extrayendo datos para {ticker} desde {start_date} hasta {end_date}')
+        data = yf.download(ticker, start=start_date, end=end_date)
+        logging.info(f'Datos extraídos exitosamente para {ticker}')
+        return data
+    except Exception as e:
+        logging.error(f'Error extrayendo datos para {ticker}: {e}')
+        return None
+
+def transform_data(data):
+    try:
+        logging.info('Transformando datos')
+        df = data[['Adj Close']].reset_index()
+        df.rename(columns={'Date': 'ds', 'Adj Close': 'y'}, inplace=True)
+        logging.info('Datos transformados exitosamente')
+        return df
+    except Exception as e:
+        logging.error(f'Error transformando datos: {e}')
+        return None
+
+def load_data(df, ticker):
+    try:
+        filename = os.path.join(data_dir, f'sandy_processed.csv')
+        logging.info(f'Guardando datos transformados en {filename}')
+        df.to_csv(filename, index=False)
+        logging.info('Datos guardados exitosamente')
+    except Exception as e:
+        logging.error(f'Error guardando datos: {e}')
+
+def etl_process(ticker, start_date, end_date):
+    data = extract_data(ticker, start_date, end_date)
+    if data is not None:
+        transformed_data = transform_data(data)
+        if transformed_data is not None:
+            load_data(transformed_data, ticker)
+            return transformed_data
+    return None
+
+table_empresas = pd.read_html('https://es.wikipedia.org/wiki/Anexo:Compa%C3%B1%C3%ADas_del_S%26P_500')
+df = table_empresas[0]
+df.to_csv('table_empresas.csv')
+
+ticker =  'MMM', 'AOS', 'ABT', 'ABBV', 'ACN', 'ATVI', 'ADM', 'ADBE', 'ADP', 'AAP', 'AES', 'AFL', 'A', 'APD', 'AKAM', 'ALK', 'ALB', 'ARE', 'ALGN', 'ALLE', 'LNT', 'ALL','GOOGL', 'GOOG', 'MO', 'AMZN', 'AMCR', 'AMD', 'AEE', 'AAL', 'AEP', 'AXP', 'AIG', 'AMT', 'AWK', 'AMP', 'ABC', 'AME', 'AMGN', 'APH', 'ADI', 'ANSS', 'AON', 'AP', 'AAPL','AMAT', 'APTV', 'ACGL', 'ANET', 'AJG', 'AIZ', 'T', 'ATO', 'ADSK' ,'AZO', 'AVB', 'AVY', 'BKR', 'BALL', 'BAC', 'BBWI', 'BAX', 'BDX', 'WRB', 'BRK.B', 'BBY', 'BIO', 'TECH','BIIB', 'BLK', 'BK', 'BA', 'BKNG', 'BWA', 'BXP', 'BSX', 'BMY', 'AVGO', 'BR', 'BRO', 'BF.B', 'CHRW', 'CDNS', 'CZR', 'CPT', 'CPB', 'COF', 'CAH', 'KMX', 'CCL','CARR', 'CTLT', 'CAT', 'CBOE', 'CBRE', 'CDW', 'CE', 'CNC', 'CNP', 'CDAY', 'CF', 'CRL', 'SCHW' ,'CHTR', 'CVX', 'CMG', 'CB', 'CHD','CI', 'CINF', 'CTAS', 'CSCO','C', 'CFG', 'CLX', 'CME', 'CMS', 'KO', 'CTSH', 'CL', 'CMCSA', 'CMA', 'CAG', 'COP', 'ED' , 'STZ', 'CEG', 'COO', 'CPRT', 'GLW', 'CTVA', 'CSGP', 'COST' , 'CTRA', 'CCI', 'CSX', 'CMI', 'CVS', 'DHI', 'DHR', 'DRI', 'DVA', 'DE', 'DAL', 'XRAY', 'DVN', 'DXCM', 'FANG', 'DLR', 'DFS', 'DISH', 'DIS', 'DG', 'DLTR', 'D', 'DPZ', 'DOV', 'DOW', 'DTE', 'DUK' ,'DD', 'DXC', 'EMN', 'ETN', 'EBAY', 'ECL', 'EIX', 'EW', 'EA', 'ELV', 'LLY', 'EMR', 'ENPH', 'ETR', 'EOG', 'EPAM', 'EQT', 'EFX', 'EQIX', 'EQR', 'ESS', 'EL', 'ETSY', 'RE', 'EVRG', 'ES', 'EXC', 'EXPE', 'EXPD', 'EXR', 'XOM', 'FFIV', 'FDS', 'FAST', 'FRT', 'FDX', 'FITB', 'FRC', 'FSLR', 'FE', 'FIS', 'FISV', 'FLT', 'FMC', 'F', 'FTNT', 'FTV', 'FOXA', 'FOX', 'BEN', 'FCX', 'GRMN', 'IT', 'GEHC', 'GEN', 'GNRC', 'GD', 'GE', 'GIS', 'GM', 'GPC', 'GILD', 'GL', 'GPN', 'GS', 'HAL', 'HIG', 'HAS', 'HCA', 'PEAK', 'HSIC', 'HSY', 'HES', 'HPE', 'HLT', 'HOLX', 'HD', 'HON', 'HRL', 'HST', 'HWM', 'HPQ', 'HUM', 'HBAN', 'HII', 'IBM', 'IEX', 'IDXX', 'ITW', 'ILMN', 'INCY', 'IR', 'INTC', 'ICE', 'IP', 'IPG', 'IFF', 'INTU', 'ISRG', 'IVZ', 'INVH', 'IQV', 'IRM', 'JBHT', 'JKHY', 'J', 'JNJ', 'JCI', 'JPM', 'JNPR', 'K', 'KDP', 'KEY', 'KEYS', 'KMB', 'KIM', 'KMI', 'KLAC', 'KHC', 'KR', 'LHX', 'LH', 'LRCX', 'LW', 'LVS', 'LDOS', 'LEN', 'LNC', 'LIN', 'LYV', 'LKQ', 'LMT', 'L', 'LOW', 'LUMN', 'LYB', 'MTB', 'MRO', 'MPC', 'MKTX', 'MAR', 'MMC', 'MLM', 'MAS', 'MA', 'MTCH', 'MKC', 'MCD', 'MCK', 'MDT', 'MRK', 'META', 'MET', 'MTD', 'MGM', 'MCHP', 'MU', 'MSFT', 'MAA', 'MRNA', 'MHK', 'MOH', 'TAP', 'MDLZ', 'MPWR', 'MNST', 'MCO', 'MS', 'MOS', 'MSI', 'MSCI', 'NDAQ', 'NTAP', 'NFLX', 'NWL', 'NEM', 'NWSA', 'NWS', 'NEE', 'NKE', 'NI', 'NDSN', 'NSC', 'NTRS', 'NOC', 'NCLH', 'NRG', 'NUE', 'NVDA', 'NVR', 'NXPI', 'ORLY', 'OXY' , 'ODFL', 'OMC', 'ON', 'OKE', 'ORCL', 'OGN', 'OTIS', 'PCAR', 'PKG', 'PARA', 'PH', 'PAYX', 'PAYC', 'PYPL', 'PNR', 'PEP', 'PKI', 'PFE', 'PCG', 'PM', 'PSX', 'PNW', 'PXD', 'PNC', 'POOL', 'PPG', 'PPL', 'PFG', 'PG','PGR', 'PLD', 'PRU', 'PEG', 'PTC', 'PSA', 'PHM', 'QRVO', 'PWR', 'QCOM', 'DGX', 'RL', 'RJF', 'RTX', 'O', 'REG', 'REGN', 'RF', 'RSG', 'RMD', 'RHI', 'ROK', 'ROL', 'ROP', 'ROST', 'RCL', 'SPGI', 'CRM', 'SBAC', 'SLB', 'STX', 'SEE', 'SRE', 'NOW', 'SHW', 'SBNY', 'SPG', 'SWKS', 'SJM', 'SNA', 'SEDG', 'SO', 'LUV', 'SWK', 'SBUX', 'STT', 'STLD', 'STE', 'SYK', 'SIVB', 'SYF', 'SNPS', 'SYY', 'TMUS', 'TROW', 'TTWO', 'TPR', 'TRGP', 'TGT', 'TEL', 'TDY', 'TFX', 'TER', 'TSLA', 'TXN', 'TXT', 'TMO', 'TJX', 'TSCO', 'TT', 'TDG', 'TRV', 'TRMB', 'TFC', 'TYL', 'TSN', 'USB', 'UDR', 'ULTA', 'UNP', 'UAL', 'UPS' ,'URI', 'UNH', 'UHS', 'VLO', 'VTR', 'VRSN', 'VRSK', 'VZ', 'VRTX', 'VFC', 'VTRS', 'VICI', 'V', 'VMC', 'WAB', 'WBA', 'WMT', 'WBD', 'WM', 'WAT', 'WEC', 'WFC', 'WELL', 'WST', 'WDC', 'WRK', 'WY', 'WHR', 'WMB', 'WTW', 'GWW', 'WYNN', 'XEL', 'XYL', 'YUM', 'ZBRA', 'ZBH', 'ZION', 'ZTS'
+start_date = '2024-01-01'
+end_date = '2024-03-31'
+data = etl_process(ticker, start_date, end_date)
+
---
+++ FASE-3/PROYECTO3-pyodbc.py
@@ -0,0 +1,82 @@
+# PROYECTO3 Sandy Acosta
+
+import pyodbc
+import pandas as pd
+
+
+# 1. Configuración de la conexión
+server = 'DESKTOP-IB90627'
+database = 'PROYECTO3'
+username = 'zoe12'
+password = '12345'
+connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+
+# Conexión a la base de datos
+conn = pyodbc.connect(connection_string)
+cursor = conn.cursor()
+
+# 2.Crear una tabla (si no existe)
+
+cursor.execute('''
+    IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='CompanyProfiles' AND xtype='U')
+    CREATE TABLE CompanyProfiles (
+        Symbol VARCHAR(50) NOT NULL,
+        Company VARCHAR(50),
+        Sector VARCHAR(50),
+        Headquarters VARCHAR(50),
+        FechaFundada VARCHAR(50),
+     CONSTRAINT CompanyProfiles_pk PRIMARY KEY (Symbol)
+    )
+''')
+
+cursor.execute('''
+    IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Companies' AND xtype='U')
+    CREATE TABLE Companies (
+        Date DATE,
+        Symbol VARCHAR(50),
+        Clos FLOAT,
+        PRIMARY KEY (Date, Symbol),
+        FOREIGN KEY (Symbol) REFERENCES CompanyProfiles(Symbol)
+    )
+''')
+conn.commit()
+
+# Ruta a los archivos CSV
+
+CompanyProfiles_csv= 'C:/Users/SANDY/Documents/SANDY/TIC/TALENTO-TECH/PROYECTO-ETL/PROYECTO-3/CSV/CompanyProfiles.csv'
+Companies_csv= 'C:/Users/SANDY/Documents/SANDY/TIC/TALENTO-TECH/PROYECTO-ETL/PROYECTO-3/CSV/Companies.csv'
+
+ # Lee los datos del archivo CSV en DataFrames
+CompanyProfiles_df= pd.read_csv(CompanyProfiles_csv,sep=';', quotechar='"')
+Companies_df= pd.read_csv(Companies_csv,sep=';', quotechar='"')
+
+
+# Imprime los nombres de las columnas
+print("Columnas en CompanyProfiles:")
+print(CompanyProfiles_df.columns)
+
+print("\nColumnas en Companies:")
+print(Companies_df.columns)
+
+# Cargar datos en la tabla CompanyProfiles
+for index, row in CompanyProfiles_df.iterrows():
+    cursor.execute('''
+        INSERT INTO CompanyProfiles (Symbol, Company, Sector, Headquarters, FechaFundada)
+        VALUES (?, ?, ?, ?, ?)
+    ''', row['Symbol'], row['Company'], row['Sector'], row['Headquarters'], row['FechaFundada'])
+conn.commit()
+
+# Cargar datos en la tabla Companies
+print("paso")
+for index, row in Companies_df.iterrows():
+    cursor.execute('''
+        INSERT INTO Companies (Date, Symbol,  Clos)
+        VALUES (?, ?, ?)
+    ''', row['Date'], row['Symbol'], row['Clos'] )
+conn.commit()
+
+# Cerrar la conexión
+cursor.close()
+conn.close()
+
+print("Datos cargados exitosamente en SQL Server.")
